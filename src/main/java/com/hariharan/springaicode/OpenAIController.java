d1fc92c798e9e0545ca62fa64559698a90348c90
Hariharan <hariharan9572@gmail.com>
2026-01-19 03:07:50 +0530

    working with ChatClient

--- src/main/java/com/hariharan/springaicode/OpenAIController.java
+++ src/main/java/com/hariharan/springaicode/OpenAIController.java
@@ -1,6 +1,8 @@
 package com.hariharan.springaicode;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,18 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenAIController {
 
-    private OpenAiChatModel chatModel;
+    private ChatClient chatClient;
 
     public OpenAIController(OpenAiChatModel chatModel){
-        this.chatModel = chatModel;
+        this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping("/api/{message}")
-    public String getAnswer(@PathVariable String message){
+    public ResponseEntity<String> getAnswer(@PathVariable String message){
 
-        String response = chatModel.call(message);
+        String response = chatClient
+                .prompt(message)
+                .call()
+                .content();
 
-        return response;
+        return ResponseEntity.ok(response);
     }
 
 }
