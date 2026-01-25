538c38f4a96ee723d89318d358b709ca05f39dc3
Hariharan <hariharan9572@gmail.com>
2026-01-26 00:32:25 +0530

    Redis Vector Store Implementation

--- src/main/java/com/hariharan/springaicode/AppConfig.java
+++ src/main/java/com/hariharan/springaicode/AppConfig.java
@@ -3,17 +3,26 @@ package com.hariharan.springaicode;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPooled;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VectorStore vectorStore(EmbeddingModel embeddingModel){
-        return PgVectorStore.builder(, embeddingModel)
+    public JedisPooled jedisPooled(){
+        return new JedisPooled("localhost", 6379);
+    }
 
+    @Bean
+    public VectorStore vectorStore(JedisPooled jedisPooled, EmbeddingModel embeddingModel){
+        return RedisVectorStore.builder(jedisPooled, embeddingModel)
+                .indexName("product-index")
+                .initializeSchema(true)
                 .build();
 
     }
