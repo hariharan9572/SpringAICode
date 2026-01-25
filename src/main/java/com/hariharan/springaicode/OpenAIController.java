fae5ee8f73c3d5abd2723e5f8345d82cba2dffb6
Hariharan <hariharan9572@gmail.com>
2026-01-25 20:15:46 +0530

    Spring AI Memory Advisor

--- src/main/java/com/hariharan/springaicode/OpenAIController.java
+++ src/main/java/com/hariharan/springaicode/OpenAIController.java
@@ -1,6 +1,9 @@
 package com.hariharan.springaicode;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +20,14 @@ public class OpenAIController {
         this.chatClient = ChatClient.create(chatModel);
     }*/
 
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
     public OpenAIController(ChatClient.Builder builder){
-        this.chatClient = builder.build();
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor
+                        .builder(chatMemory)
+                        .build())
+                .build();
     }
 
     @GetMapping("/api/{message}")
